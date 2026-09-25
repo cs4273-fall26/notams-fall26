e998078d2728b84618c39dcad448443d37a45ad7
Ricky Vincent <ricky.t.vincent-1@ou.edu>
2026-09-25 17:53:35 -0500

    updated file location and name

--- src/main/java/UserNotamInput.java
+++ src/main/java/UserNotamInput.java
@@ -1,20 +1,22 @@
+package src.main.java;
 import java.util.Scanner;
 
 // This program collects flight information from the user and prints it out.
-public class input {
-    public static void main(String[] var0){
+public class UserNotamInput {
+    public static void main(String[] var0) {
         Scanner inputScanner = new Scanner(System.in);
         System.out.println("Please enter your departure airport: ");
         String startAirport = inputScanner.nextLine();
         System.out.println("Please enter your destination airport: ");
         String endAirport = inputScanner.nextLine();
         System.out.println("Please enter your departure date: ");
-        String departureDate = inputScanner.nextLine(); 
+        String departureDate = inputScanner.nextLine();
         System.out.println("Please enter your departure time: ");
         String departureTime = inputScanner.nextLine();
         System.out.println("Is your flight direct or a waypoint: ");
         String flightType = inputScanner.nextLine();
-        System.out.println(startAirport + " " + endAirport + " " +  departureDate + " " + departureTime + " " + flightType);  
+        System.out.println(
+                startAirport + " " + endAirport + " " + departureDate + " " + departureTime + " " + flightType);
         inputScanner.close();
     }
 }
